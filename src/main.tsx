5fdba83ba19e14b87fdc0a0c0a2b66e552727ee4
SanjayKumar3110 <sanjaykumar785231@gmail.com>
2026-04-03 21:55:49 +0530

    feat: separate desktop and mobile versions

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,10 @@
-import { StrictMode, useState, useEffect } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import MobileApp from './mobile/MobileApp.tsx'
-
-function PlatformSwitcher() {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
-
-  useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth < 768);
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  return isMobile ? <MobileApp /> : <App />;
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PlatformSwitcher />
+    <App />
   </StrictMode>,
 )
